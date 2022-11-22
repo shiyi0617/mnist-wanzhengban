8ffb06c429255358514f3960a954e95fd3af51c1
shiyi0617 <118649453+shiyi0617@users.noreply.github.com>
2022-11-22 13:55:20 +0800

    Add files via upload

---
+++ mnist/duqushuju.py
@@ -0,0 +1,8 @@
+import sys,os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+(x_train,t_train),(x_test,y_test)=load_mnist(flatten=True,normalize=False)
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(x_test.shape)
